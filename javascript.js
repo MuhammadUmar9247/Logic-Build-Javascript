eeb6cee2770d89b104c48c8f72dbe83b92624ac3
Muhammad Umar <amnakhan7028@gmail.com>
2025-12-25 09:38:31 -0800

    Logic Building 🦾

---
+++ javascript.js
@@ -0,0 +1,77 @@
+// Question Number 1
+
+// var num = prompt("Please Enter a Number");
+// num = Number(num);
+
+// document.write("Number :" +num+"<br>");
+// document.write("Round OFF!"+Math.round(num)+"<br>");
+// document.write("Floor Valuee!"+Math.floor(num)+"<br>");
+// document.write("Ceil Value!"+Math.ceil(num)+"<br>");
+
+// Question Number 2
+
+// var num = prompt("Enter a negative floating number:")
+// num = Number(num);
+
+// document.write("Number "+num+"<br>");
+// document.write("Round Off Value: "+Math.round(num)+"<br>");
+// document.write("Floor Value: "+ Math.floor(num)+"<br>");
+// document.write("Ceil Value: "+ Math.ceil(num)+"<br>");
+
+
+// Question Number 3
+
+// var num = prompt("Enter a Number");
+// num = Number(num);
+
+// document.write("Absolute Value"+Math.abs(num));
+
+// Question Number 4
+
+// var dice = Math.floor(Math.random()*6)+1;
+// document.write("Dice Value"+dice);
+
+
+// Question Number 5
+
+
+// var toss = Math.floor(Math.random()*2);
+
+// if(toss === 0){
+//  document.write("Heads");
+// }else{
+//     document.write("Tails");
+// }
+
+
+// Question Number 6
+
+// var randomnum = Math.floor(Math.random()*100)+1;
+// document.write("Random Number"+randomnum);
+
+
+// Question Number 7
+
+// var weight = prompt("Enter Your Weight");
+// var parsedWeight=parseFloat(weight);
+
+// document.write("Your Weight is"+parsedWeight + " kgs..." + "<br>")
+
+
+// Question Number 8
+
+
+// var scret = Math.floor(Math.random()*18)+1;
+// var userNum =prompt("Guess a Number between 1 and 10 :" );
+// userNum =Number(userNum);
+
+// if (userNum === scret){
+//     document.write("Congtrats.... You Guess Accurate Number!")
+// } else{
+//     document.write("You Guess Wrong Number!");
+// }
+
+
+
+
+
